511440f329700bd628b9228ce2b87337ae576f6b
Artem <artem.yakymchuk@gmail.com>
2018-07-01 18:10:28 +0300

    add relations

--- app/MarketHistory.php
+++ app/MarketHistory.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class MarketHistory extends Model
 {
     protected $fillable = ['currency_id', 'unit_currency_id','rate_source_id','market_cap','price'];
+
+
+
+    public function currency()
+    {
+        return $this->belongsTo(Currency::class);
+    }
+    public function rate_source()
+    {
+        return $this->belongsTo(RateSource::class);
+    }
+    public function unit_currency()
+    {
+        return $this->hasOne(Currency::class,'id','unit_currency_id');
+    }
 }

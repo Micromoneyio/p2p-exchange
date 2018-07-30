e3c2c1014a7ba1e7a7801bfb082d7dd37a4a12ab
Oleg Globin <ginc.xyz@gmail.com>
2018-07-30 07:17:58 +0300

    add sync and observer

---
+++ app/Jobs/SyncRequestJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\BpmModule;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SyncRequestJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @param  string $entity
+     * @return void
+     */
+    public function handle(string $entity, $model)
+    {
+        $module = new BpmModule();
+        switch ($entity) {
+            case 'user':  $module->contact($model); break;
+            case 'order': $module->order($model);   break;
+        }
+    }
+}

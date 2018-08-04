47c1a9e418d95a93290a0a4c8f4af5ddf57f1f5a
Michael <michael.cleveroad@gmail.com>
2018-08-04 17:47:01 +0300

    added relations to deals

--- app/Http/Resources/DealsResource.php
+++ app/Http/Resources/DealsResource.php
@@ -14,6 +14,8 @@ class DealsResource extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => DealResource::collection($this->collection)
+        ];
     }
 }

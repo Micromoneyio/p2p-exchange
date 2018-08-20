22d6720fbb586586e84e0a46c234cf8838bfd667
Artem Yakymchuk <artem.yakymchuk@footshop.cz>
2018-08-20 19:35:28 +0300

    deal cancel action

--- routes/api.php
+++ routes/api.php
@@ -46,6 +46,7 @@ Route::group(['middleware' => ['jwt.auth']], function ($router) {
     Route::post('orders/filter','OrderController@filter');
     Route::post('deals/{deal}/pay', 'DealController@pay');
     Route::post('deals/{deal}/release', 'DealController@release');
+    Route::post('deals/{deal}/cancel', 'DealController@cancel');
 });
 
 Route::get('user/verify/{verification_code}', 'AuthController@verifyUser');

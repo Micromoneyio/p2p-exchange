343671881e68b01c529207c1abb8a2c99131fbef
Artem Yakymchuk <artem.yakymchuk@footshop.cz>
2018-08-22 21:41:49 +0300

    fixes

--- app/CryptoModule.php
+++ app/CryptoModule.php
@@ -93,7 +93,7 @@ class CryptoModule
         try{
         $res = $this->client->request(
             'POST',
-            $this->api_url . '/transactions/' . $this->currency . '/fee-included',
+            $this->api_url . '/transactions/' . $this->currency . '/fee-not-included',
             [
                 'body'=>\GuzzleHttp\json_encode([
                     'to'=>$accountTo,

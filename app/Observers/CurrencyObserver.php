1fd5413c4058d431e6eececec63eb5df4211d672
Oleg Globin <ginc.xyz@gmail.com>
2018-09-25 21:10:17 +0300

    add currrency model

---
+++ app/Observers/CurrencyObserver.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Observers;
+
+use App\Currency;
+
+class CurrencyObserver
+{
+    /**
+     * Handle to the order "created" event.
+     *
+     * @param  \App\Order  $order
+     * @return void
+     */
+    public function created(Currency $currency)
+    {
+        BpmRequestJob::dispatch($currency);
+    }
+
+    /**
+     * Handle the order "updated" event.
+     *
+     * @param  \App\Order  $order
+     * @return void
+     */
+    public function updated(Currency $currency)
+    {
+        BpmRequestJob::dispatch($currency);
+    }
+
+    /**
+     * Handle the order "deleted" event.
+     *
+     * @param  \App\Order  $order
+     * @return void
+     */
+    public function deleted(Currency $currency)
+    {
+        //
+    }
+}

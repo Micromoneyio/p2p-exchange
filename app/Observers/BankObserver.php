6b12f4d746f775a9da4bcbb9b0d0e5766f20c918
Oleg Globin <ginc.xyz@gmail.com>
2018-09-28 00:55:37 +0300

    fixes

--- app/Observers/BankObserver.php
+++ app/Observers/BankObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Bank;
+use App\Jobs\BpmRequestJob;
 
 class BankObserver
 {
--- app/Observers/DealStageObserver.php
+++ app/Observers/DealStageObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\DealStage;
+use App\Jobs\BpmRequestJob;
 
 class DealStageObserver
 {

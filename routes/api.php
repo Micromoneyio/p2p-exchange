255942b7ec828160c403fa065de33ef652fc396f
Artem Yakymchuk <artem.yakymchuk@footshop.cz>
2018-10-02 20:06:46 +0300

    social auth, password reset

--- routes/api.php
+++ routes/api.php
@@ -49,7 +49,7 @@ Route::group(['middleware' => ['jwt.auth']], function ($router) {
 });
 
 Route::get('user/verify/{verification_code}', 'AuthController@verifyUser');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.request');
+Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.request');
 Route::post('password/reset', 'AuthController@resetPassword')->name('password.reset');
 
 Route::get('tst', function () {

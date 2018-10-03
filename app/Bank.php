5c8549cd4e5795de75440adb1f279d45093d5e19
Oleg Globin <ginc.xyz@gmail.com>
2018-10-03 22:57:19 +0300

    fixes

--- app/Bank.php
+++ app/Bank.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bank extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'bpm_id'];
 
     public function assets() {
         return $this->hasMany('App\Asset');

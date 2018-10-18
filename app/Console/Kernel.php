f8da8f5b89913f6496e1e3b6a9285cf206097384
Artem Yakymchuk <artem.yakymchuk@footshop.cz>
2018-10-18 18:45:44 +0300

    currency fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -43,18 +43,22 @@ class Kernel extends ConsoleKernel
                         'market_cap' => $result[0]['market_cap_'.strtolower($item->symbol)],
                         'price' => $result[0]['price_'.strtolower($item->symbol)],
                     ]);
-                    if ($item->crypto){
-                        $item->usd_price = $result[0]['price_usd'];
-                    }else{
-                        $item->usd_price = $fiat_usd_course['rates'][$item->symbol] ?? 0;
-                        if ($item->usd_price){
-                            $item->usd_price = 1 / $item->usd_price;
-                        }
-                    }
-                    $item->save();
                 }
 
             }
+            foreach ($currencies as $item){
+                if ($item->crypto==1){
+                    $result = json_decode(file_get_contents(getenv('API_URL') . strtolower($item->name)),true);
+                    $item->usd_price = $result[0]['price_usd'];
+                }else{
+                    $item->usd_price = $fiat_usd_course['rates'][$item->symbol] ?? 0;
+                    if ($item->usd_price){
+                        $item->usd_price = 1 / $item->usd_price;
+                    }
+                }
+                $item->save();
+            }
+            
         })->hourly();
     }
 

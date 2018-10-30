df853d1024cbdae48754cc183f4d7ce2d06d5038
Mihail Kudryashev <uamedwed@gmail.com>
2018-10-30 21:33:56 +0300

    Re-create UsersFacrory.php file

---
+++ database/factories/UsersFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Users::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'telegram' => $faker->word,
+        'email' => $faker->email,
+        'password' => $faker->password,
+        'default_currency_id' => '1',
+        'min_rank' => $faker->randomDigitNotNull,
+        'rank' => $faker->randomDigitNotNull,
+        'deals_count'  => $faker->randomDigitNotNull
+    ];
+});
